2044144e9752352e665c45f6782aee0c4f22f3bc
Finbarr O'Callaghan <finbarr.ocallaghan@gmail.com>
2014-04-07 22:50:59 +0100

    stylesheets etc

--- nvd3stuff.js
+++ nvd3stuff.js
@@ -7,14 +7,24 @@ d3.json('weightdata.json', function(json) {
                 values: json.data
             };
         });
+
+        chart.margin({left: 80 });
         chart.xAxis.tickFormat(function(d) {
-            return d3.time.format('%y-%m-%d')(new Date(d * 1000));
+            return d3.time.format('%d-%m-%y')(new Date(d * 1000));
         })
-        chart.yAxis.tickFormat(d3.format(',.2f'));
-        chart.x2Axis.tickFormat(function(d) {
-            return d3.time.format('%y-%m-%d')(new Date(d * 1000));
+        chart.yAxis.axisLabel('weight [kg]').tickFormat(d3.format(',.2f'));
+
+        chart.x2Axis.axisLabel('date').tickFormat(function(d) {
+            return d3.time.format('%d-%m-%y')(new Date(d * 1000));
         })
+
         chart.y2Axis.tickFormat(d3.format(',.2f'));
+
+        chart.tooltipContent(function(key, y, e, graph) {
+            return '<p>' + y + '</p>' + '<p>' + e + '</p>'
+        })
+
+
         d3.select('#chart svg').datum(varb).transition().duration(500).call(chart);
         nv.utils.windowResize(chart.update);
         return chart;
